d39436e70a31eb66d4d87361a6c9405552a8584e
Jonathan Tarnate <jstarnate16@gmail.com>
2022-06-15 21:26:24 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,16 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import postCssColorMod from 'postcss-color-mod-function';
-import postCssPresetEnv from 'postcss-preset-env';
+import react from '@vitejs/plugin-react';
 import { join } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [reactRefresh()],
-    css: {
-        postcss: {
-            plugins: [
-                postCssPresetEnv({
-                    stage: 3,
-                    features: {
-                        'nesting-rules': true,
-                    },
-                }),
-                postCssColorMod(),
-            ],
-        },
+  plugins: [react()],
+  server: {
+    open: false,
+  },
+  resolve: {
+    alias: {
+      '@': join(__dirname, 'src'),
     },
-    server: {
-        open: false,
-        port: 3000,
-    },
-    resolve: {
-        alias: {
-            '~': join(__dirname, 'src'),
-            '@': join(__dirname, 'src/components'),
-        },
-    },
+  },
 });
